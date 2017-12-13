a5e658b69deb44ee1125322f1b5badf53e9626cb
Wes Reid <bwreid@gmail.com>
2017-12-13 11:33:33 -0800

    Add FireworkItem Component
    - Create FireworkItem component that takes a firework and renders
      only the title
    - Loop over fireworks data in the FireworksList

---
+++ src/components/FireworkItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FireworkItem = ({ firework }) => (
+  <div>
+    <h2>{ firework.name }</h2>
+  </div>
+);
+
+export default FireworkItem;
--- src/components/FireworksList.js
+++ src/components/FireworksList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import FireworkItem from './FireworkItem'
 
-const FireworksList = ({ fireworks }) => (
-  <div className='container'>
-    <div className='row'>
-      <div className='col'>
-        <h1>All Fireworks</h1>
-        { JSON.stringify(fireworks) }
+const FireworksList = ({ fireworks }) => {
+  const list = fireworks.map(firework => <FireworkItem key={ firework.id } firework={ firework } />)
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1>All Fireworks</h1>
+          <hr/>
+          { list }
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+}
 
 export default FireworksList;
