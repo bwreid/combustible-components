fa8c41a49219f957843d579882f2577f0d8c618e
Wes Reid <bwreid@gmail.com>
2017-12-13 11:21:37 -0800

    Add Jumbotron
    - Add Bootstrap Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="jumbotron jumbotron-fluid bg-danger text-white">
+        <div className="container">
+          <h1 className="display-3">Combustible Components</h1>
+        </div>
       </div>
     );
   }
