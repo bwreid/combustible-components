e32aad5a7f6efd50e46e68646606abc68e4cacef
Wes Reid <bwreid@gmail.com>
2017-12-13 11:26:26 -0800

    Add FireworksList Component
    - Create components/ folder
    - Create FireworksList Component that has a title
    - Include FireworksList in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import FireworksList from './components/FireworksList'
 
 class App extends Component {
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid bg-danger text-white">
-        <div className="container">
-          <h1 className="display-3">Combustible Components</h1>
+      <main>
+        <div className="jumbotron jumbotron-fluid bg-danger text-white">
+          <div className="container">
+            <h1 className="display-3">Combustible Components</h1>
+          </div>
         </div>
-      </div>
+        <FireworksList />
+      </main>
     );
   }
 }
---
+++ src/components/FireworksList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FireworksList = () => (
+  <div className='container'>
+    <div className='row'>
+      <div className='col'>
+        <h1>All Fireworks</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default FireworksList;
